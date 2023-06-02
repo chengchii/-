bce34bd00bf1264b21653b503017aaad1ad89b74
chengchii <118353364+chengchii@users.noreply.github.com>
2023-06-02 14:23:20 +0800

    Update client-bigoldtwo.py

--- client-bigoldtwo.py
+++ client-bigoldtwo.py
@@ -1,5 +1,5 @@
 import socket
-
+import time
 HOST = '127.0.0.1'
 PORT = 5000
 
@@ -8,37 +8,36 @@ client_socket.connect((HOST, PORT))
 
 
 def slipper(choice):
-    if len(choice) == 0:
-        return True
     if len(choice) == 1 and choice[0] == 'p':
         return False
-    
+    if len(choice) == 0:
+        return True
     for i in range(len(choice)):
         if not choice[i].isdigit():
-            #print(choice[i],choice[i].isdigit())
             return True
     else:
         return False
 
 recv = {
-    '1' : '請輸入牌型:(若要第一張 --> 1 第二張 --> 2 以此類推，中間以空格間隔，若要pass請輸入p )',
+    '1' : '請輸入牌型:(若要第一張 --> 1 第二張 --> 2 以此類推，中間以空格間隔)',
     '2' : '請重新輸入:',
+    '3' : '遊戲結束'
 }
 # 接收 "Please enter your name (player1/player2/player3):" 消息
 message = client_socket.recv(1024).decode().strip()
 print(message)
 
-# 发送玩家名称
-#player_name = input()
-#client_socket.send(player_name.encode())
 c= True
-# 接收游戏数据
 while True:
     data = client_socket.recv(1024).decode().strip()
     if data in recv:
         data = recv[data]
         print(data)
-        choice = input().lower().split() # 接收玩家选择并发送到服务器
+        if data == '遊戲結束':
+            time.sleep(2)
+            client_socket.close()
+            break
+        choice = input().lower().split()
         c = slipper(choice) 
         while c:
             choice = input('請重新輸入:').lower().split()
